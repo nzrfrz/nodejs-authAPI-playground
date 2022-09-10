dc4118dd17f8803dd92782c3fcd93229be76d73e
unknown <nzr.frz@gmail.com>
2022-09-10 13:56:17 +0700

    change method to get @ generate access token

--- src/routes/generateAccessToken.js
+++ src/routes/generateAccessToken.js
@@ -4,7 +4,7 @@ import { AccessTokenGenerator } from "../controllers/accessTokenGenerator.js";
 const router = express.Router();
 
 export const GenerateAccessToken = (app) => {
-    const generateAccessToken = router.post("/generate/accessToken", AccessTokenGenerator);
+    const generateAccessToken = router.get("/generate/accessToken", AccessTokenGenerator);
 
     app.use("/auth", generateAccessToken);
 };
